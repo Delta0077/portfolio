46713480ea6aa3d06014d021e4172e8a60e93e01
Delta0077 <adas0262@gmail.com>
2022-09-27 02:34:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from "./components/Services";
 import About from "./components/About";
 import Work from "./components/Work";
 import Footer from "./components/Footer";
+import './styles/style.css'
 export default function App() {
   return (
     <>
