a5114218eead4db76278418730dd8290af7f70da
Delta0077 <adas0262@gmail.com>
2022-09-28 02:40:51 +0530

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ export default function Intro() {
   return (
     <section className="intro" id="home">
       <h1 className="section__title section__title--intro">
-        Hi, I am <strong className="bold">Aditya Das</strong>
+        Hi, I am <strong>Aditya Das</strong>
       </h1>
       <p className="section__subtitle section__subtitle--intro">
         front-end dev
