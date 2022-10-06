00a95b59a9b7e0fc9b3cea59d21d37e7b141dd15
Delta0077 <adas0262@gmail.com>
2022-10-06 13:37:09 +0530

    Hamburger implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import Services from "./components/Services";
 import About from "./components/About";
 import Work from "./components/Work";
 import Footer from "./components/Footer";
-import './styles/style.css'
+import "./styles/style.css";
 export default function App() {
   return (
     <>
-      <Header />
+      <Navbar />
       <Intro />
       <Services />
       <About />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "../styles/Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+export default function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
+  function handleClick() {
+    setNavOpen((prevState) => !prevState);
+  }
+  return (
+    <header>
+    <div className="logo">
+    <img src="/devadi.svg" alt=""></img>
+  </div>
+      <nav className="nav">
+        <div className={navOpen ? "navbar showMenu" : "nav__item"}>
+          <ul className="nav__list">
+            <li className="nav__item  showMenu">
+              <a href="#home" className="nav__link">
+                Home
+              </a>
+            </li>
+            <li className="nav__item  showMenu">
+              <a href="#services" className="nav__link">
+                My Services
+              </a>
+            </li>
+            <li className="nav__item  showMenu">
+              <a href="#about" className="nav__link">
+                About Me
+              </a>
+            </li>
+            <li className="nav__item  showMenu">
+              <a href="#work" className="nav__link">
+                My Work
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      {/* hamburger menu start  */}
+      <div className="hamburger-menu">
+        <a
+          href="#"
+          onClick={handleClick}
+          className={`navbar ${navOpen ? "showMenu" : ""}`}
+        >
+          <GiHamburgerMenu className="hamburger-icon" />
+        </a>
+      </div>
+    </header>
+  );
+}
