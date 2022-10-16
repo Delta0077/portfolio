2ba898f84211a897e4e1486bdf786bcc66594322
Delta0077 <adas0262@gmail.com>
2022-10-17 00:12:10 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -7,30 +7,17 @@ export default function Services() {
       <h2 className="section__title section__title--services">What I do</h2>
       <div className="services">
         <div className="service">
-          <h3>Design + Development</h3>
+          <h3>Design</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Proficient in making web-page design using prototyping tools like
+            Figma & Adobe XD.
           </p>
         </div>
         <div className="service">
-          <h3>E-Commerce</h3>
+          <h3>Development</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-        <div className="service">
-          <h3>WordPress</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+            Capable of making responsive and interactive websites using
+            <strong>HTML5,CSS & React js</strong>
           </p>
         </div>
         {/* Close service */}
